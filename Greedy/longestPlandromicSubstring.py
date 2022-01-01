c39859047400dea24b655dd782b5088f517c35ac
Abhishek Malviya <abfeb8@gmail.com>
2022-01-01 18:49:21 +0530

    v2

--- Greedy/longestPlandromicSubstring.py
+++ Greedy/longestPlandromicSubstring.py
@@ -1,15 +1,26 @@
 # https://leetcode.com/problems/longest-palindromic-substring/
 
 '''
-Time complexity O(n)
-Space complexity O(n^3)
+Time complexity O(n^2)
+Space complexity O(1)
 '''
 
 
+def getMaxLenPalindrome(p1, p2, s, maxLenPalindrome):
+    while(p1 >= 0 and p2 < len(s)):
+        if(s[p1] == s[p2]):
+            p1 -= 1
+            p2 += 1
+        else:
+            break
+    if(len(maxLenPalindrome) < p2-p1-1):
+        maxLenPalindrome = s[p1+1:p2]
+
+    return maxLenPalindrome
+
+
 def solve(s) -> str:
-
-    n = len(s)
-
+    # string will always have a unit length palindrome 
     maxLenPalindrome = s[0]
 
     for i, c in enumerate(s[:-1]):
@@ -18,27 +29,13 @@ def solve(s) -> str:
             # check for even palindrome
             p1 = i-1
             p2 = i+2
-            while(p1 >= 0 and p2 < n):
-                if(s[p1] == s[p2]):
-                    p1 -= 1
-                    p2 += 1
-                else:
-                    break
-            if(len(maxLenPalindrome) < p2-p1-1):
-                maxLenPalindrome = s[p1+1:p2]
+            maxLenPalindrome = getMaxLenPalindrome(p1, p2, s, maxLenPalindrome)
 
         if(i-1 >= 0 and s[i-1] == s[i+1]):
             # check for odd palindrome
             p1 = i-1
             p2 = i+1
-            while(p1 >= 0 and p2 < n):
-                if(s[p1] == s[p2]):
-                    p1 -= 1
-                    p2 += 1
-                else:
-                    break
-            if(len(maxLenPalindrome) < p2-p1-1):
-                maxLenPalindrome = s[p1+1:p2]
+            maxLenPalindrome = getMaxLenPalindrome(p1, p2, s, maxLenPalindrome)
 
     return maxLenPalindrome
 
