734b0153150dc9b8570f7e2e1c008e5e298d24dc
Abhishek Malviya <abfeb8@gmail.com>
2022-01-01 17:00:57 +0530

    accepted

--- Two Pointer/longest_substring_withodut_repeating.py
+++ Two Pointer/longest_substring_withodut_repeating.py
@@ -4,9 +4,37 @@
 Time complexity O(n)
 Space complexity O(n)
 '''
+
+
 def solve(s: str) -> int:
-    
-    return 
+    n = len(s)
+
+    # if the string is empty or has only one char
+    if n < 2:
+        return n
+
+    # initialising a dict to store visited characters
+    check = dict()
+
+    # initialising pointer
+    p1 = 0
+
+    # initialising maxLen variable
+    maxLen = 0
+
+    # iterating over s
+    for p2, char in enumerate(s):
+
+        if(char in check and check[char] >= p1):
+            maxLen = max(maxLen, p2-p1)
+            p1 = check[char]+1
+
+        check[char] = p2
+
+    # if no repeting char is found in the end of the string
+    maxLen = max(maxLen, p2-p1+1)
+
+    return maxLen
 
 
 # !driver code
