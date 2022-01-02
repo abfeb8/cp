09744424b9890ecb7ee67b190dd9c315bcae4231
Abhishek Malviya <abfeb8@gmail.com>
2022-01-02 23:55:24 +0530

    clean code but still slow

--- zigZagConversion.py
+++ zigZagConversion.py
@@ -1,25 +1,10 @@
 '''
-Time complexity O(n)
-Space complexity O(1)
+Time complexity O(n) -> Since every elemnet is visited only once
+Space complexity O(1) -> storing only result
 '''
 
-
-def getLine(s: str, startIndex: int, firstJump: int, secondJump: int, n: int):
-    line = ""
-    curIndex = startIndex
-    turn = 0
-    while (curIndex < len(s)):
-        if (turn & 1 == 0 and firstJump != 0):
-            line += s[curIndex]
-            curIndex += firstJump
-        elif(secondJump != 0):
-            line += s[curIndex]
-            curIndex += secondJump
-        turn += 1
-    return line
-
-
 def solve(s: str, numRows: int):
+    # edge case 
     if numRows == 1:
         return s
 
@@ -30,7 +15,16 @@ def solve(s: str, numRows: int):
     for i in range(numRows):
         firstJump = 2*i
         secondJump = gap-firstJump
-        result += getLine(s, i, secondJump, firstJump, numRows)
+        curIndex = i
+        turn = 0
+        while (curIndex < len(s)):
+            if (turn & 1 == 1 and firstJump != 0):
+                result += s[curIndex]
+                curIndex += firstJump
+            elif(secondJump != 0):
+                result += s[curIndex]
+                curIndex += secondJump
+            turn += 1
 
     return result
 
