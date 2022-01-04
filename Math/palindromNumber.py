f14419fbd9d4cfff099e3997693d0dd02f0e0c2b
Abhishek Malviya <abfeb8@gmail.com>
2022-01-04 22:11:33 +0530

    fast solution

--- Math/palindromNumber.py
+++ Math/palindromNumber.py
@@ -5,34 +5,21 @@ Space complexity O(N)
 
 
 def solve(x: int):
-    # negative number can't be palindrome
-    if x < 0:
+    # negative number and egde cases can't be palindrome
+    if x < 0 or (x % 10 == 0 and x != 0):
         return False
 
-    # all single digit numbers are palindrome
-    if x < 10:
+    # getting last half of the integer
+    y = 0
+    while(y < x):
+        y = (y*10)+(x % 10)
+        x //= 10
+
+    # checking palindrome
+    if x == y or x == y//10:
         return True
 
-    l = list()
-    # n -> index of last element of l
-    n = 0
-    while(x > 9):
-        l.append(x % 10)
-        x //= 10
-        n += 1
-
-    # adding last element to the list
-    l.append(x)
-    
-    # checking palindrome
-    p = 0
-    while (p < n):
-        if(l[p] != l[n]):
-            return False
-        p += 1
-        n -= 1
-
-    return True
+    return False
 
 
 # !driver code
