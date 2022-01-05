7867acd46f9fcaa386d80e29e78440717394d6dc
Abhishek Malviya <abfeb8@gmail.com>
2022-01-05 22:56:00 +0530

    accepted but slow

---
+++ removeElement.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/remove-element/
+
+'''
+Time complexity O(n)
+Space complexity O()
+'''
+
+
+def solve(nums: list, val: int):
+    k = 0
+    l = 0
+    for i, c in enumerate(nums):
+        if c == val:
+            k += 1
+        else:
+            nums[i-k] = c
+            l += 1
+
+    print(nums[:l])
+    return l
+
+
+# !driver code
+if __name__ == "__main__":
+    nums = list(map(int, input().split(",")))
+    val = int(input())
+    solve(nums, val)
