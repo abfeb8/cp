2d59d2f3f0dad655e890152f57769d465a505505
Abhishek Malviya <abfeb8@gmail.com>
2022-03-03 10:17:37 +0530

    accepted java solution

---
+++ Stack/validParenthese.java
@@ -0,0 +1,47 @@
+package Stack;
+
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currBracket = s.charAt(i);
+            if (currBracket == '(' || currBracket == '[' || currBracket == '{') {
+                stack.add(currBracket);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char lastBracket = stack.peek();
+                switch (lastBracket) {
+                    case '(':
+                        if (currBracket == ')') {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case '[':
+                        if (currBracket == ']') {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case '{':
+                        if (currBracket == '}') {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                        break;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+}
