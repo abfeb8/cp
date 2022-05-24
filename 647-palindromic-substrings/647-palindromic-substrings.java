c7376bb3c97a86187b716f77b8ea20defb8de496
Abhishek Malviya <abfeb8@gmail.com>
2022-05-24 19:02:53 +0530

    Time: 3 ms (94.50%), Space: 41.8 MB (79.51%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -3,28 +3,35 @@ class Solution {
     public int countSubstrings(String s) {
         char[] arr = s.toCharArray();
         cache = new int[arr.length+1];
-        for(int len = 1; len <= arr.length; len++){
-            cache[len] = cache[len-1] + countNewPalindrom(arr, len);
+        
+        for(int indx = 0; indx < arr.length; indx++){
+            cache[indx+1] = cache[indx] + countNewPalindrom(arr, indx);
+            // System.out.println(indx+1 + "->" + cache[indx+1]);
         }
         return cache[arr.length];
     }
     
-    private int countNewPalindrom(char[] arr, int end){
-        int result = 0;
+    private int countNewPalindrom(char[] arr, int index){
+        int result = 0, l = index, r = index;
         
-        for(int start = 0; start < end; start++){
-            if(isPalindrome(start, end-1, arr))
-                result++;
+        // count odd palindrome
+        while(l >= 0 && r < arr.length){
+            if(arr[l--] != arr[r++])
+                break;
+            result++;
+        }
+        
+        //count even palindrome
+        l = index-1; r = index;
+        while(l >= 0 && r < arr.length){
+            if(arr[l--] != arr[r++])
+                break;
+            result++;
         }
         
         return result;
     }
     
-    private boolean isPalindrome(int l, int r, char[] arr){
-        while(l<r)
-            if(arr[l++] != arr[r--])
-                return false;
-        
-        return true;
-    }
+    
+    
 }
