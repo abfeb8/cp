986551aa1493b48b8b1ba099459ec11f8f185e0c
Abhishek Malviya <abfeb8@gmail.com>
2022-06-13 10:29:43 +0530

    Time: 6 ms (40.34%), Space: 44.5 MB (24.79%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -3,15 +3,29 @@ class Solution {
     Map<String, Integer> cache = new HashMap<>(); 
     
     public int minimumTotal(List<List<Integer>> triangle) {
-        // var dp = new ArrayList<List<Integer>>();
-        // dp.add(Arrays.asList(triangle.get(0)));
+        var dp = new ArrayList<List<Integer>>();
+        dp.add(triangle.get(0));
         
         int n = triangle.size();
-        return helper(triangle, n, 0, 0);
+        
+        for(int r = 1; r<n; r++){
+            Integer[] arr = new Integer[r+1];
+            for(int c = 0; c<=r; c++){
+               arr[c] = Math.min(dp.get(r-1).get(Math.max(0, c-1)), dp.get(r-1).get(Math.min(r-1, c))) + triangle.get(r).get(c);
+            }
+            dp.add(Arrays.asList(arr));
+        }
+        
+        int result = Integer.MAX_VALUE;
+        for(int i : dp.get(n-1)){
+            result = Math.min(result, i);
+        }
+        
+        return result;
         
     }
     
-    private int helper(List<List<Integer>> triangle, int n, int row, int col){
+    private int topDown(List<List<Integer>> triangle, int n, int row, int col){
         if(row == n-1){
             return triangle.get(row).get(col);
         }
@@ -21,9 +35,8 @@ class Solution {
             return cache.get(key);
         
         int curVal = triangle.get(row).get(col);
-        int minPath = Math.min(helper(triangle, n, row+1, col), helper(triangle, n, row+1, col+1)) + curVal;
+        int minPath = Math.min(topDown(triangle, n, row+1, col), topDown(triangle, n, row+1, col+1)) + curVal;
         cache.put(key, minPath);
         return minPath;
-    
     }
 }
