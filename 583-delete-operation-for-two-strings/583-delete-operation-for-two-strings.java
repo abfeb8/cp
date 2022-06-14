66ad6810aa76a5260fb47fd38b31267f613e3c67
Abhishek Malviya <abfeb8@gmail.com>
2022-06-14 20:55:47 +0530

    Time: 11 ms (75.83%), Space: 48 MB (5.51%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -4,10 +4,20 @@ class Solution {
     
     public int minDistance(String word1, String word2) {
         char[] a =  word1.toCharArray(), b = word2.toCharArray();
-        return a.length + b.length - (count(a, b, 0, 0) * 2);
+        int[][] dp = new int[a.length+1][b.length+1];
+        
+        // i and j represents length of a and b respectively
+        for(int i = 1; i<=a.length; i++){
+            for(int j = 1; j<=b.length; j++){
+                dp[i][j] = a[i-1] == b[j-1] ? dp[i-1][j-1] + 1 : Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        // return a.length + b.length - (topDown(a, b, 0, 0) * 2);
+        return a.length + b.length - (2 * dp[a.length][b.length]);
     }
     
-    private int count(char[] a, char[] b, int p1, int p2){
+    private int topDown(char[] a, char[] b, int p1, int p2){
         int n1 = a.length, n2 = b.length;
         if(p1>=n1 || p2>=n2){
             return 0;
@@ -19,9 +29,9 @@ class Solution {
         
         int temp = 0;
         if(a[p1] == b[p2]){
-            temp = count(a, b, p1+1, p2+1) + 1;
+            temp = topDown(a, b, p1+1, p2+1) + 1;
         }else{
-            temp = Math.max(count(a, b, p1+1, p2), count(a, b, p1, p2+1));
+            temp = Math.max(topDown(a, b, p1+1, p2), topDown(a, b, p1, p2+1));
         }
         cache.put(key, temp);
         return temp;
